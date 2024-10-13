6f3b41960e279c04bfc39f1fe7ab0a65c8f15d2c
krzysztofj12345 <krzysztofj12345@mailo.com>
2024-10-13 15:33:05 -0400

    funct

--- demo/src/main/java/com/example/concurrency/LambdaExamples.java
+++ demo/src/main/java/com/example/concurrency/LambdaExamples.java
@@ -1,6 +1,7 @@
 package com.example.concurrency;
 
 import java.util.*;
+import java.util.function.* ;
 
 public class LambdaExamples {
     public static void main(String[] args) { 
@@ -10,5 +11,20 @@ public class LambdaExamples {
         numbers.add(8);
         numbers.add(1);
         numbers.forEach( (n) -> { System.out.println(n); } );
+
+        exercise1() ;
+        exercise2() ;
     }
+
+    // lambda expression to check if a given string is empty.
+    static void exercise1() {
+        Predicate<String> isEmptyString = str -> str.isEmpty();
+        System.out.println( isEmptyString.test("ala") ) ;
+    }
+
+    // Sum 2 numbers
+    static void exercise2() {
+        Function<String, String> fun = s1 -> s1 + 2;
+        System.out.println( fun.apply("ala") ) ;
+        }
 }
--- demo/src/main/java/com/example/concurrency/StreamsDemo.java
+++ demo/src/main/java/com/example/concurrency/StreamsDemo.java
@@ -3,6 +3,7 @@ package com.example.concurrency;
 import java.util.*;
 import java.util.stream.* ;
 import java.util.function.*;
+import java.math.BigDecimal;
 
 public class StreamsDemo {
     public static void main( String[] args ) {
@@ -43,6 +44,7 @@ public class StreamsDemo {
         exercise9() ;
         exercise10() ;
         exercise11() ;
+        exercise12() ;
     }
 
     static int exercise1( Integer[] values, int k ) {
@@ -139,4 +141,47 @@ public class StreamsDemo {
         List<Integer> result = numbers.stream().filter( c -> c %2 == 0).map( a -> a*a ).collect( Collectors.toList()) ;
         System.out.println(result);
     }
+
+    // Group by the name + Count or Sum the Qty.
+    static void exercise12() {
+        //3 apple, 2 banana, others 1
+        List<Item> items = Arrays.asList(
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 20, new BigDecimal("19.99")),
+                new Item("orang", 10, new BigDecimal("29.99")),
+                new Item("watermelon", 10, new BigDecimal("29.99")),
+                new Item("papaya", 20, new BigDecimal("9.99")),
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 10, new BigDecimal("19.99")),
+                new Item("apple", 20, new BigDecimal("9.99"))
+        );
+
+        Map<String, Long> counting = items.stream().collect( Collectors.groupingBy(Item::getName, Collectors.counting()));
+        System.out.println(counting);
+
+        Map<String, Integer> result = items.stream().collect( Collectors.groupingBy(Item::getName, Collectors.summingInt(Item::getQty)));
+        System.out.println(result);
+    }    
+}
+
+class Item {
+
+    private String name;
+    private int qty;
+    private BigDecimal price;
+    public Item(String name, int qty, BigDecimal price) {
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getQty() {
+        return qty;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    
 }
